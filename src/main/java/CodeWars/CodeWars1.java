e73ab581e5c09ac3d2e5fe1b98a54aeb375152aa
Peter <pgkutc@gmail.com>
2021-02-18 10:40:08 -0500

    Added camelCase Challenge

--- src/main/java/CodeWars/CodeWars1.java
+++ src/main/java/CodeWars/CodeWars1.java
@@ -9,11 +9,29 @@ public class CodeWars1 {
         String[] keys = {"1", "ABC2", "DEF3", "GHI4", "JKL5", "MNO6", "PQRS7", "TUV8", "WXYZ9", "*", " 0", "#"};
         int count = 0;
 
-        for(char c : phrase.toCharArray()) {
+        for (char c : phrase.toCharArray()) {
             for (String s : keys) {
                 count += s.indexOf(c) + 1;
             }
         }
         return count;
+    }
+
+    public static String camelCase(String input) {
+        char[] characters = input.toCharArray();
+        StringBuilder result = new StringBuilder();
+
+        for(char chars : characters) {
+            if(Character.isUpperCase(chars)) {
+                result.append(" " + chars);
+            } else {
+                result.append(chars);
+            }
+        }
+        return result.toString();
+    }
+
+    public static String camelCase2(String input) {
+        return input.replaceAll("([A-Z])", " $1");
     }
 }
--- src/test/java/CodeWars/CodeWars1Test.java
+++ src/test/java/CodeWars/CodeWars1Test.java
@@ -8,8 +8,18 @@ public class CodeWars1Test {
     CodeWars1 codeWars1 = new CodeWars1();
 
     @Test
-    public void simpleTest() {
+    public void pressesTest() {
         assertEquals(9, codeWars1.presses("LOL"));
         assertEquals(13, codeWars1.presses("HOW R U"));
+    }
+
+    @Test
+    public void camelCaseTests() {
+        assertEquals( "Incorrect", "camel Casing", codeWars1.camelCase("camelCasing"));
+        assertEquals( "Incorrect", "camel Casing Test", codeWars1.camelCase("camelCasingTest"));
+        assertEquals( "Incorrect", "camelcasingtest", codeWars1.camelCase("camelcasingtest"));
+        assertEquals( "Incorrect", "camel Casing", codeWars1.camelCase2("camelCasing"));
+        assertEquals( "Incorrect", "camel Casing Test", codeWars1.camelCase2("camelCasingTest"));
+        assertEquals( "Incorrect", "camelcasingtest", codeWars1.camelCase2("camelcasingtest"));
     }
 }
